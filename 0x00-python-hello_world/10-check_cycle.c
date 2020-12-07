2f5bef90ab616c41260b25e5ff00daa8adf1986b
Soledad Frechou <solefrechou@hotmail.com>
2020-12-07 18:30:03 +0000

    Add task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,22 +10,22 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *temp;
+	listint_t *temp;
 
 	if (list == NULL)
 	{
 		return (0);
 	}
 
-    while (list)
-    {
-        temp = list;
-        list = list->next;
+	while (list)
+	{
+		temp = list;
+		list = list->next;
 
-        if (temp <= list)
-        {
-            return (1);
-        }
-    }
-    return (0);
+		if (temp <= list)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
