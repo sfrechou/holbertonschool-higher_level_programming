1818428ec5987147b513d17b47f98eeab2025a1d
Soledad Frechou <solefrechou@hotmail.com>
2020-12-17 21:46:24 +0000

    Add file 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for key, value2 in list(a_dictionary.items()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
