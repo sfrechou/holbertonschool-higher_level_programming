46f0d44911c63ce5adf0f8fd1a1db7adc7d6cb6a
Soledad Frechou <solefrechou@hotmail.com>
2021-01-05 18:41:30 +0000

    Add files

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    __size = None
+
+    def __init__(self, size=0):
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    __size = None
+
+    def __init__(self, size=0):
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size * self.__size
