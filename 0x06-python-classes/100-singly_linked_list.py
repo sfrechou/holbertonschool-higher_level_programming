aff657b3bbba79a8e1b6194c02ea746f1fa8b186
Soledad Frechou <solefrechou@hotmail.com>
2021-01-06 01:26:02 +0000

    Add file

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -7,8 +7,8 @@ class Node:
 
     def __init__(self, data, next_node=None):
         """instantiation with data and next_node"""
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
