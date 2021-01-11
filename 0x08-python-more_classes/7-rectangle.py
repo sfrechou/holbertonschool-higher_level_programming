15c59bca64abb351cfb762ee3a78222e05c5eb53
Soledad Frechou <solefrechou@hotmail.com>
2021-01-11 22:09:58 +0000

    Add fix

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
         if self.__width != 0 and self.__height != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    sq += "#"
+                    sq += str(self.print_symbol)
                 if i < self.__height - 1:
                     sq += "\n"
         return sq
