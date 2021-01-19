acf305c8f23d517460a38593a00bff985914b723
Soledad Frechou <solefrechou@hotmail.com>
2021-01-19 21:21:10 +0000

    Add fix 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ Module for task 4.
 
 def inherits_from(obj, a_class):
     """checks if object is exact instance of class"""
-    if issubclass(a_class, type(obj)):
+    if type(obj) == a_class:
         return False
     else:
-        return True
+        return isinstance(obj, a_class)
