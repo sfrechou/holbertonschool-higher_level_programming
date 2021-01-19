d5881ae46e479e70996bb89b0000d08e1b409c1f
Soledad Frechou <solefrechou@hotmail.com>
2021-01-19 22:17:36 +0000

    Add file

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for task 13.
+"""
+
+
+def add_attribute(obj, name, value):
+    """add attribute when possible"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add a new attribute")
+    else:
+        setattr(obj, name, value)
