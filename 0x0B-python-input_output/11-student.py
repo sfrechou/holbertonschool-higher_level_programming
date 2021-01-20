3161c3583d3743f7cad851bb522dbc89677ce191
Soledad Frechou <solefrechou@hotmail.com>
2021-01-20 21:49:01 +0000

    Add 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for task 10 - Student to JSON with filter"""
+
+
+class Student:
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation for Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieve dictionary"""
+        my_dict = {}
+        if attrs is not None:
+            for i in attrs:
+                if type(i) is str:
+                    if i in self.__dict__:
+                        my_dict[i] = self.__dict__[i]
+            return my_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for i in json:
+            self.__dict__[i] = json[i]
