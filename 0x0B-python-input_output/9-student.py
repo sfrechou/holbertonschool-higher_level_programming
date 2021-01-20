4a2bdc2646f419f1e1cad276e9fdfb9d11b39b47
Soledad Frechou <solefrechou@hotmail.com>
2021-01-20 21:05:01 +0000

    Add file 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """retrieve dictionary"""
         return self.__dict__
