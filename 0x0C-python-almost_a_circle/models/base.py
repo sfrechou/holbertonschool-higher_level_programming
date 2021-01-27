6949a76951034503a9806f44e91ea80fb9d46ad7
Soledad Frechou <solefrechou@hotmail.com>
2021-01-27 13:48:34 +0000

    Add base fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,7 +40,7 @@ class Base:
     def from_json_string(json_string):
         """Returns the list of the JSON string representation"""
         if json_string is None or len(json_string) == 0:
-            return "[]"
+            return []
         return json.loads(json_string)
 
     @classmethod
