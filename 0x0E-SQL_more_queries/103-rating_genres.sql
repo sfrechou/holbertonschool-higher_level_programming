ec8f3eaa12bd16f5d885f5657564f04043138732
Soledad Frechou <solefrechou@hotmail.com>
2021-03-04 02:46:24 +0000

    Add 103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,1 +1,9 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_show_ratings
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY SUM(tv_show_ratings.rate) DESC;
