63a6fc46f80f718d93e95cc382b447816f2e95c2
Soledad Frechou <solefrechou@hotmail.com>
2021-03-22 19:46:02 +0000

    Add fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,10 @@
 if (process.argv.length < 4) {
   console.log(1);
 } else {
-  const numbers = process.argv.slice(2);
+  let numbers = process.argv.slice(2);
   numbers.sort(function (a, b) { return a - b; });
-  const uniq = [...new Set(numbers)];
-  const uniqNums = Array.from(uniq);
-  const index = uniqNums.length;
+  let uniq = [...new Set(numbers)];
+  let uniqNums = Array.from(uniq);
+  let index = uniqNums.length;
   console.log(uniqNums[index - 2]);
 }
