095deadc5a89e24ead4d809624e4caa9259124d4
Soledad Frechou <solefrechou@hotmail.com>
2021-03-22 19:01:56 +0000

    Add fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,39 +4,9 @@ if (process.argv.length < 4) {
   console.log(1);
 } else {
   const numbers = process.argv.slice(2);
-  let biggest = numbers[0];
-  let secondBigg = numbers[0];
-  let i;
-  const negative = numbers.every(function (e) {
-    return e < 0;
-  });
-  if (negative === true) {
-    console.log('aqui');
-    for (i = 0; i < numbers.length; i++) {
-      if (numbers[i] < biggest) {
-        biggest = numbers[i];
-      }
-    }
-    const indexBigg = numbers.indexOf(biggest);
-    numbers.splice(indexBigg, 1);
-    for (i = 0; i < numbers.length; i++) {
-      if (numbers[i] < secondBigg) {
-        secondBigg = numbers[i];
-      }
-    }
-  } else {
-    for (i = 0; i < numbers.length; i++) {
-      if (numbers[i] > biggest) {
-        biggest = numbers[i];
-      }
-    }
-    const indexBigg = numbers.indexOf(biggest);
-    numbers.splice(indexBigg, 1);
-    for (i = 0; i < numbers.length; i++) {
-      if (numbers[i] > secondBigg) {
-        secondBigg = numbers[i];
-      }
-    }
-  }
-  console.log(secondBigg);
+  numbers.sort(function (a, b) { return a - b; });
+  const uniq = [...new Set(numbers)];
+  const uniqNums = Array.from(uniq);
+  const index = uniqNums.length;
+  console.log(uniqNums[index - 2]);
 }
