d333e3f1334c42a8a770f8fa40e73f4e9dffc8f6
Soledad Frechou <solefrechou@hotmail.com>
2021-03-22 16:00:30 +0000

    Add 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// js script thatt prints x times “C is fun”
+let i;
+if (!parseInt(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
