893b931494f23c36a5a30f0174955ba0a099688c
Soledad Frechou <solefrechou@hotmail.com>
2021-03-22 17:18:04 +0000

    Add fix 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 // js script thatt computes and prints a factorial
-if (!parseInt(process.argv[2])) {
-  console.log(1);
-} else {
-  const n = parseInt(process.argv[2]);
-  function factorial (n) {
+
+const n = parseInt(process.argv[2]);
+function factorial (n) {
+  if (isNaN(n) === true) {
+    return (1);
+  } else {
     if (n === 0) {
       return (1);
     }
-    return n * factorial(n - 1);
+    return (n * factorial(n - 1));
   }
-  console.log(factorial(n));
 }
+console.log(factorial(n));
