61de49b2d9f6db747957f1c1a0170cb24f28b351
Soledad Frechou <solefrechou@hotmail.com>
2021-03-22 19:52:21 +0000

    Add fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // js script thatt searches the second biggest integer in the list of arguments
 if (process.argv.length < 4) {
-  console.log(1);
+  console.log(0);
 } else {
-  let numbers = process.argv.slice(2);
+  const numbers = process.argv.slice(2);
   numbers.sort(function (a, b) { return a - b; });
-  let uniq = [...new Set(numbers)];
-  let uniqNums = Array.from(uniq);
-  let index = uniqNums.length;
+  const uniq = [...new Set(numbers)];
+  const uniqNums = Array.from(uniq);
+  const index = uniqNums.length;
   console.log(uniqNums[index - 2]);
 }
