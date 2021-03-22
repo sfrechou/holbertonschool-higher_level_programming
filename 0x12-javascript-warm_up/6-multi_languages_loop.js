46842c5668a262dfd08df29cfdafbb91a66d6869
Soledad Frechou <solefrechou@hotmail.com>
2021-03-22 15:55:36 +0000

    Add 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// js script thatt prints 3 lines but by using an array of string and a loop
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; i < 3; i++) {
+  console.log(lines[i]);
+}
