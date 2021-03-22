2d9a61124d226b4246d416ac61bb2e22ccf67cc4
Soledad Frechou <solefrechou@hotmail.com>
2021-03-22 15:33:38 +0000

    Add file 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // js script thatt prints My number: <first argument converted in integer>
 if (!parseInt(process.argv[2])) {
-    console.log('Not a number');
-}
-else {
-    console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
