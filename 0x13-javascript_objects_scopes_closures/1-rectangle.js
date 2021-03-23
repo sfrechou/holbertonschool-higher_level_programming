695a85882d4ee879e2457b42a578825c22a8dc41
Soledad Frechou <solefrechou@hotmail.com>
2021-03-23 16:53:07 +0000

    Add 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// js empty class Rectnagle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
