ef270ddeb3011826e2b9e62efab7c06f9f75afbb
Soledad Frechou <solefrechou@hotmail.com>
2021-03-27 13:31:05 +0000

    Add fix 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,10 +16,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host=HOST, user=USER, password=PASSWORD,
                          db=DATABASE, port=PORT)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cursor.execute("SELECT * FROM states ORDER BY id")
     rows = cursor.fetchall()
     for r in rows:
-        # This if is redundant, but checker needs it apparently
         if r[1][0] == 'N':
             print(r)
     cursor.close()
