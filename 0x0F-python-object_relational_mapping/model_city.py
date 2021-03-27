e0e3802dafd909adc1f9e64ed5cf69b5ecb403d4
Soledad Frechou <solefrechou@hotmail.com>
2021-03-27 14:14:55 +0000

    Add 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,8 +3,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
 
 class City(Base):
     """Class City declaration"""
