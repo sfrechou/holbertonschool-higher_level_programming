d831a9dc2f2e0eeb1a8f007a89d879563c4f0d7e
Soledad Frechou <solefrechou@hotmail.com>
2021-04-08 21:30:45 +0000

    100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI $1 | head -n 1 | cut -f2 -d " "
+curl -s -o /dev/null -w "%{http_code}" $1
