2cdbb58498e8e829f5e44e79d107ea81e4579213
Soledad Frechou <solefrechou@hotmail.com>
2021-04-09 16:38:50 +0000

    Add

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ import requests
 if __name__ == "__main__":
     request = requests.get('https://intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}"
-        .format(type(request.text), request.text))
+          .format(type(request.text), request.text))
