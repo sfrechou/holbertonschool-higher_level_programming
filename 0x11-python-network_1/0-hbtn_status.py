b4a496e7d422a027fc1e35fdc303e27c6886707b
Soledad Frechou <solefrechou@hotmail.com>
2021-04-09 16:35:59 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,9 @@
 import urllib.request
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
-    utf = content.decode('utf-8')
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: \
-{}".format(type(content), content, utf))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        utf = content.decode('utf-8')
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: \
+    {}".format(type(content), content, utf))
