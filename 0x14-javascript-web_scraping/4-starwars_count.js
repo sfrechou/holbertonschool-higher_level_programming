c63720ae4032315c235d87ddf84a9fcba633793a
Soledad Frechou <solefrechou@hotmail.com>
2021-05-04 20:30:56 +0000

    Add 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -12,7 +12,7 @@ request(url, function (error, response, body) {
     for (let k = 0; k < results.length; k++) {
       const chars = results[k].characters;
       for (let i = 0; i < chars.length; i++) {
-        if (chars[i] === 'https://swapi-api.hbtn.io/api/people/18/') {
+        if (chars[i].includes('/18/')) {
           j++;
         }
       }
