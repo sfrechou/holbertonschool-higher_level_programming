59da8eb8d0fcf4195becabe5f02a5e5b14cc9666
Soledad Frechou <solefrechou@hotmail.com>
2021-05-04 19:20:08 +0000

    Add 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const argu = process.argv;
 const url = argu[2];
-const https = require('https');
-const request = https.get(url, function (response) {
-  console.log('code: ' + response.statusCode);
-});
-
-request.on('error', function (error) {
-  console.error('code: ' + error.status);
+const request = require('request');
+request(url, function (error, response) {
+  console.log('code:', response && response.statusCode);
+  if (error) {
+    return console.log(error);
+  }
 });
