7f152070064c9fd966927939928a05b8b9736995
Soledad Frechou <solefrechou@hotmail.com>
2021-05-04 23:30:27 +0000

    Add 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,12 +3,13 @@ const argu = process.argv;
 const url = argu[2];
 const request = require('request');
 
+const dict = {};
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const results = JSON.parse(body);
-    const dict = {};
+
     let currentId = results[0].userId;
     let done = 0;
     for (let i = 0; i < results.length; i++) {
@@ -26,7 +27,7 @@ request(url, function (error, response, body) {
       }
     }
     dict[results[results.length - 1].userId] = done;
-    console.log(dict);
   }
+  console.log(dict);
 }
 );
