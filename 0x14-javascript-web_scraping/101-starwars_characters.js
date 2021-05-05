be03b9c1bd368e98101ffe147c8b160e5418f6c7
Soledad Frechou <solefrechou@hotmail.com>
2021-05-05 02:03:43 +0000

    Add 102

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,20 +4,32 @@ const movieId = argu[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 const request = require('request');
 
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (error) {
+        reject(console.log(error));
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+// Usage:
+
+async function main (chars) {
+  for (let j = 0; j < chars.length; j++) {
+    const res = await doRequest(chars[j]);
+    console.log(res);
+  }
+}
+
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const chars = JSON.parse(body).characters;
-    for (let i = 0; i < chars.length; i++) {
-      request(chars[i], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          const char = JSON.parse(body);
-          console.log(char.name);
-        }
-      });
-    }
+    main(chars);
   }
 });
