a2315793ff4efd49aee78573fc94661b5a16670c
Soledad Frechou <solefrechou@hotmail.com>
2021-05-05 00:24:19 +0000

    Add 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,34 +1,23 @@
 #!/usr/bin/node
-const url = 'https://jsonplaceholder.typicode.com/todos';
+const argu = process.argv;
+const url = argu[2];
 const request = require('request');
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const results = JSON.parse(body);
     const dict = {};
-    let currentId = results[0].userId;
-    let done = 0;
+    const results = JSON.parse(body);
     for (let i = 0; i < results.length; i++) {
-      if (currentId === results[i].userId) {
-        if (results[i].completed === true) {
-          done++;
+      if (results[i].completed === true) {
+        if (results[i].userId in dict) {
+          dict[results[i].userId] += 1;
+        } else {
+          dict[results[i].userId] = 1;
         }
-      } else {
-        if (done !== 0 && i > 0) {
-          dict[results[i - 1].userId] = done;
-          done = 0;
-          currentId = results[i].userId;
-          i--;
-        }
-      }
-      if (i === results.length - 1)
-      {
-        dict[results[results.length - 1].userId] = done;
       }
     }
     console.log(dict);
   }
-}
-);
+});
